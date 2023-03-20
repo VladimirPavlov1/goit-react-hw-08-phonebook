ff4e7f4591d9cd6ad096c93ec9a325166e548942
VladimirPavlov1 <106485364+VladimirPavlov1@users.noreply.github.com>
2023-03-20 07:24:40 +0000

    Deploying to gh-pages from @ VladimirPavlov1/goit-react-hw-08-phonebook@c1f01367a7afb437165e0e9167658a6f8693cd4b 🚀

---
+++ static/js/304.de90fda2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[304],{304:function(e,n,t){t.r(n),t.d(n,{default:function(){return a}});var c,r=t(168),i=t(7402).Z.div(c||(c=(0,r.Z)(["\n    min-height: calc(100vh - 50px);\n    display: flex;\n    align-items: center;\n    justify-content: center;\n    "]))),l=t(184);function a(){return(0,l.jsx)(i,{children:(0,l.jsx)("h1",{children:"Welcome"})})}}}]);
+//# sourceMappingURL=304.de90fda2.chunk.js.map
