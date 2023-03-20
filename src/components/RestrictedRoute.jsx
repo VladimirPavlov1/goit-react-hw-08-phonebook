3ef9365d533825f333640206ac12d5c9d4b4e37e
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-03-20 10:22:23 +0200

    _

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -3,6 +3,6 @@ import { useAuth } from 'hooks/hooks';
 
 export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn } = useAuth();
-  console.log(isLoggedIn);
+ 
   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
